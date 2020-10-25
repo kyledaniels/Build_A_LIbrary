acff825d25ed55076ed2b3b4a3515229a5450bc2
kyle67 <daniels.kyle@gmail.com>
2020-10-25 15:45:34 -0400

    created getter and setter for Media class

--- app.js
+++ app.js
@@ -1,7 +1,26 @@
 class Media {
+
     constructor(keyOne){
+
         this._title=title;
         this._isCheckedOut=false;
         this._ratings=[];
+
+    }
+
+    get title(){
+        return this._title;
+    }
+
+    get isCheckedOut(){
+        return this._isCheckedOut;
+    }
+
+    get ratings(){
+        return this._ratings;
+    }
+
+    set isCheckedOut(){
+        this._isCheckedOut=false
     }
 }
