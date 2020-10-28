f09e90ee88f5c88b551a3500503a9ba7ee852b98
kyle67 <daniels.kyle@gmail.com>
2020-10-27 21:58:26 -0400

    set properties for Movie class

--- app.js
+++ app.js
@@ -59,7 +59,16 @@ class Book extends Media {
 class Movie extends Media {
       constructor(director,title,runTime){
           super(title)
-          this._title=title;
+          this._director=director;
           this._runTime=runTime;
       }
+
+      get director(){
+          return this._director;
+      }
+
+      get runTime(){
+          return this._runTime;
+      }
+      getAverag
 }
